5164e61f3b1f414b82f49f7be86c3cd955a11be7
nathanquinteiro <nathan.quinteiro@gmail.com>
2016-10-06 20:07:19 +0200

    Configure project to use with Google Firebase, created a function to push dummy  messages to Firebase database

--- app/src/main/java/ch/epfl/sweng/project/WelcomeActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/WelcomeActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,11 +15,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static ch.epfl.sweng.project.R.attr.title;
+
 public class WelcomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button testButton;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +61,33 @@ public class WelcomeActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        testButton = (Button) findViewById(R.id.testButton);
+        testButton.setOnClickListener(testClickListener);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+    }
+
+    View.OnClickListener testClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            writeNewPost();
+        }
+    };
+
+
+    
+    private void writeNewPost() {
+        String key = mDatabase.child("messages").push().getKey();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", "result");
+        childUpdates.put("/messages/" + key, result);
+
+        mDatabase.updateChildren(childUpdates);
     }
 
     @Override
@@ -64,8 +107,10 @@ public class WelcomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_create) {
+        if (id == R.id.nav_log_in) {
             // Handle the camera action
+        } else if (id == R.id.nav_sign_up) {
+
         } else if (id == R.id.nav_share) {
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
@@ -75,7 +120,7 @@ public class WelcomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_contact) {
             Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                 "mailto",getResources().getString(R.string.company_mail), null));
-            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+            startActivity(Intent.createChooser(intent, getResources().getString(R.string.contact_message)));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
