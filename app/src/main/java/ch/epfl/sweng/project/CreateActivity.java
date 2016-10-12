6265da1c155652e0f79e409b4466ba7052568223
BenoitCretegny <benoit.cretegny@epfl.ch>
2016-10-12 18:31:11 +0200

    added date and time picker

--- app/src/main/java/ch/epfl/sweng/project/CreateActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/CreateActivity.java
@@ -1,30 +1,64 @@
 package ch.epfl.sweng.project;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.text.format.DateFormat;
 
+//import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class CreateActivity extends AppCompatActivity{
 
+    TextView dateTextView;
+    TextView timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_activity);
 
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        Calendar calendar = Calendar.getInstance();
+        String currentDateString =
+                calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault()) + " " +
+                calendar.get(Calendar.DAY_OF_MONTH) + "." +
+                calendar.get(Calendar.MONTH) + "." +
+                calendar.get(Calendar.YEAR);
+        dateTextView.setText(currentDateString);
+
+        timeTextView = (TextView) findViewById(R.id.dateTextView);
+        String currentTimeString =
+                calendar.get(Calendar.HOUR) + ":" +
+                calendar.get(Calendar.MINUTE);
+        timeTextView.setText(currentTimeString);
+
+
 
     }
 
 
+
+
+    /*
+    long date = System.currentTimeMillis();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd, yyyy h:mm a");
+    String dateString = sdf.format(date);
+    dateTextView.setText(dateString);
+    */
 
 
     public static class TimePickerFragment extends DialogFragment
@@ -51,6 +85,32 @@ public class CreateActivity extends AppCompatActivity{
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 }
