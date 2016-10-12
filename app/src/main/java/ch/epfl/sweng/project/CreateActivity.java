aeb84e03fba7cac6f79d3bc1762c8bc2ff7fd7bb
BenoitCretegny <benoit.cretegny@epfl.ch>
2016-10-12 17:08:02 +0200

    test

--- app/src/main/java/ch/epfl/sweng/project/CreateActivity.java
+++ app/src/main/java/ch/epfl/sweng/project/CreateActivity.java
@@ -1,11 +1,17 @@
 package ch.epfl.sweng.project;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TimePicker;
 
-/**
- * Created by nathan on 07.10.16.
- */
+import java.util.Calendar;
+
 
 public class CreateActivity extends AppCompatActivity{
 
@@ -18,5 +24,33 @@ public class CreateActivity extends AppCompatActivity{
 
     }
 
+
+
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
 
 }
