c192279970a754a9ae99789d41b3e2961538a81f
Artem <rollmaxidom@gmail.com>
2016-10-21 12:51:35 +0200

    adds test espresso for Activity Preview and Display Activity, can't cheeck behavior because of access's absence

--- app/src/androidTest/java/ch/epfl/sweng/project/WelcomeActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/WelcomeActivityTest.java
@@ -2,12 +2,19 @@ package ch.epfl.sweng.project;
 
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.LinearLayout;
+
+import java.util.Calendar;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static ch.epfl.sweng.project.R.id.activityPreviewsLayout;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 
@@ -19,20 +26,28 @@ import ch.epfl.sweng.project.uiobjects.ActivityPreview;
 public class WelcomeActivityTest extends ActivityInstrumentationTestCase2<WelcomeActivity> {
     public WelcomeActivityTest() {
         super(WelcomeActivity.class);
+
     }
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+
     }
 
     public void testCanPrewiewActivityBeClicked() {
         getActivity();
-
-        onData(allOf(instanceOf(ActivityPreview.class)))
+        onData(instanceOf(ActivityPreview.class))
                 .perform(click());
-        //onView(allOf(isAssignableFrom(ActivityPreview.class)))
-          //      .perform(click());
+        onView(withId(R.id.eventTitle)).check(matches(isDisplayed()));
+        onView(withId(R.id.eventDescription)).check(matches(isDisplayed()));
+
+
+        /*onData(allOf(instanceOf(ActivityPreview.class)))
+                .perform(click());
+
+        onView(allOf(isAssignableFrom(ActivityPreview.class)))
+                .perform(click());*/
     }
 }
