c0eb42b1a73eff5ae333b80f1d25d8ba6b5e3dcd
nathanquinteiro <nathan.quinteiro@gmail.com>
2016-10-26 10:58:16 +0200

    Correct error in test

--- app/src/androidTest/java/ch/epfl/sweng/project/DataProviderTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/DataProviderTest.java
@@ -42,7 +42,7 @@ public final class DataProviderTest {
 
 
         DataProvider dp = new DataProvider();
-        String uid = dp.pushActivity(dA);c
+        String uid = dp.pushActivity(dA);
 
 
         dp.getActivityFromUid(new DataProvider.DataProviderListenerActivities() {
