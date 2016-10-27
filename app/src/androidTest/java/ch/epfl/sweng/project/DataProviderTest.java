63891677c4fd3b070cbf6c7a85efd9683258cb32
Jérémie <jeremie.mayor@gmail.com>
2016-10-27 16:23:16 +0200

    add test dataProvider userEnrolledInActivity

--- app/src/androidTest/java/ch/epfl/sweng/project/DataProviderTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/DataProviderTest.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.project;
 
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +23,60 @@ public final class DataProviderTest {
 
     Boolean testFinished = false;
 
+
+    @Test
+    public void userEnrolledInActivityTest()
+    {
+
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+
+        startDate.set(Calendar.YEAR, 2016);
+        startDate.set(Calendar.MONTH, 11);
+
+        final DeboxActivity dA = new DeboxActivity("zdkasKKLD", "Nathan",
+                "Football in UNIL sport center", "Indoor football tournaments open to every student " +
+                "of UNIL and EPFL, teams are formed 15 minutes before and tournament consists of 11 " +
+                "minutes games",
+                startDate,
+                endDate,
+                122.01,
+                121.0213,
+                "Sports");
+
+        DataProvider dp = new DataProvider();
+
+        // push this activity
+        String uid = dp.pushActivity(dA);
+        /*
+        check is user is already enrolled in this activity (must be no beacause activity has just
+        be created.
+         */
+        dp.userEnrolledInActivity(new DataProvider.DataProviderListener() {
+            @Override
+            public void getActivity(DeboxActivity activity) {
+
+            }
+
+            @Override
+            public void getActivities(List<DeboxActivity> activitiesList) {
+
+            }
+
+            @Override
+            public void getIfEnrolled(boolean result) {
+
+                assertThat(result,is(false));
+                testFinished=true;
+            }
+        }, uid);
+
+        while (!testFinished) ;
+    }
+
+    /**
+     *  Test if method userEnrooledInActivity works properly
+     */
     @Test
     public void DeboxActivityConstructorAndMethod() {
         Calendar startDate = Calendar.getInstance();
@@ -49,12 +104,12 @@ public final class DataProviderTest {
             @Override
             public void getActivity(DeboxActivity activity) {
                 assertThat(activity.getDescription(), is(dA.getDescription()));
-                assertThat(activity.getTitle(), is(activity.getTitle()));
-                assertThat(activity.getOrganizer(), is(activity.getOrganizer()));
-                assertThat(activity.getLocation(), is(activity.getLocation()));
-                assertThat(activity.getCategory(), is(activity.getCategory()));
-                assertThat(activity.getTimeStart(), is(activity.getTimeStart()));
-                assertThat(activity.getTimeEnd(), is(activity.getTimeEnd()));
+                assertThat(activity.getTitle(), is(dA.getTitle()));
+                assertThat(activity.getOrganizer(), is(dA.getOrganizer()));
+                assertThat(activity.getLocation(), is(dA.getLocation()));
+                assertThat(activity.getCategory(), is(dA.getCategory()));
+                assertThat(activity.getTimeStart(), is(dA.getTimeStart()));
+                assertThat(activity.getTimeEnd(), is(dA.getTimeEnd()));
                 testFinished = true;
             }
 
@@ -69,9 +124,12 @@ public final class DataProviderTest {
             }
         }, uid);
 
+        Log.e("clé",uid);
 
-        while (!testFinished);
+        while (!testFinished) ;
 
 
     }
+
+
 }
