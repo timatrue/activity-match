e49658309e148dc529067e890cc5621086b0d3f1
Jérémie <jeremie.mayor@gmail.com>
2016-11-13 08:24:38 +0100

    clean DataProvider

--- app/src/main/java/ch/epfl/sweng/project/DataProvider.java
+++ app/src/main/java/ch/epfl/sweng/project/DataProvider.java
@@ -50,7 +50,6 @@ public class DataProvider {
     }
 
 
-
     public static class CategoryName{
         String categoryId;
         String nameCategory;
@@ -67,7 +66,7 @@ public class DataProvider {
     }
 
     public void getAllCategories(final DataProviderListenerCategories listener) {
-        //database = FirebaseDatabase.getInstance();
+
         DatabaseReference myCategories = database.getReference("categories");
 
         myCategories.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -90,7 +89,7 @@ public class DataProvider {
 
     }
     public void getSpecifiedCategory(final DataProviderListenerCategory listener, String specifiedCategory) {
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         DatabaseReference getActivities = database.getReference("activities");
         Query getCategory = getActivities.orderByChild("category").equalTo(specifiedCategory);
 
@@ -112,8 +111,8 @@ public class DataProvider {
     }
 
     public void getAllActivities(final DataProviderListenerActivities listener) {
-        DatabaseReference myRef = database.getReference("activities");
 
+        DatabaseReference myRef = database.getReference("activities");
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -195,7 +194,7 @@ public class DataProvider {
     }
 
     public Void getActivityFromUid(final DataProviderListenerActivity listener, final String uid) {
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         DatabaseReference myRef = database.getReference("activities/" + uid);
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -220,12 +219,7 @@ public class DataProvider {
      */
     public void userEnrolledInActivity(final DataProviderListenerEnrolled listener, final String uid) {
 
-        //database = FirebaseDatabase.getInstance();
-        //user = FirebaseAuth.getInstance().getCurrentUser();
-
         String userUid = user.getUid();
-
-        //DatabaseReference myRef = database.getReference("users/"+user.getUid()+"/enrolled");
         DatabaseReference myRef = database.getReference("users/" + userUid + "/enrolled");
 
 
